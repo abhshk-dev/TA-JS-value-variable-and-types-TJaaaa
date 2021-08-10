dec9c30cf4a10641c1b4c2da48955672533bf749
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2021-08-10 17:42:02 +0530

    Block-3

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,24 +5,64 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user=prompt(`Enter Your Name`);
+alert(`User: ${user}`);
+let userName=user;
+console.log(user);
+console.log(userName);
+let age=22,isLearning=true;
+user=`John`;
+console.log(user);
+console.log(userName);
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA=Number(prompt(`Enter 1st Number`));
+let numB=Number(prompt(`Enter 2nnd Number`));
+let sum=numA+numB;
+alert(`The sum of two numbers is : ${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10;j++){
+    if(j%2===0){
+        console.log(j);
+    }
+} 
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+ 
+for(let i=10;i>=1;i=i-1){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let k='5';
+let cat=' ';
+while(k>=0){
+    
+    cat+=k;
+    
+    k=k-1;
+}
+console.log(cat);
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let count=' ',n=10;
 
-// Comparisoin
+while(n>=0){
+    if(n%2===0){
+        count+=n;
+    }
+    
+    n--;
+}
+console.log(count);
+// Comparison
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
 
@@ -36,3 +76,13 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1=Number(prompt(`Enter 1st number`));
+let num2=Number(prompt(`Enter 2nd number`));
+if(num1===num2){
+    alert(`true`);
+}
+
+else {
+    alert(`false`)
+}
